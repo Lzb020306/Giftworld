9163bcc89855268ead51d0acf0251e5f6a9582c3
lzb <76713773@qq.com>
2021-03-04 18:40:31 +0800

    礼物等级

--- src/components/gift-count.vue
+++ src/components/gift-count.vue
@@ -10,6 +10,22 @@
           {{ item }}
         </li>
       </ul>
+      <div class="gift-right">
+        <h2>三级礼物</h2>
+        <ul class="gift-third">
+          <li v-for="(item, index) in Level3" :key="index">
+            <img :src="item.Icon" />
+            <p>{{ item.name }}</p>
+          </li>
+        </ul>
+        <h2 class="four">四级礼物</h2>
+        <ul class="gift-fourth">
+          <li v-for="(item, index) in Level4" :key="index">
+            <img :src="item.Icon" />
+            <p>{{ item.name }}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -20,24 +36,29 @@ export default {
     return {
       revert: require("../assets/heifanhui.png"),
       Grade: ["一级", "二级", "三级", "四级", "五级", "六级", "七级", "八级"],
-      Level3:[],
-      Level4:[],
-      ranking:[]
+      Level3: [],
+      Level4: [],
+      ranking: [],
     };
   },
   mounted() {
-      this.$axios
+    this.$axios
       .get("./gift.json")
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         this.ranking = response.data;
-        // this.ranking.forEach((i) => {
-        // //   console.log(i.id);
-        // });
-        //   this.ranking.forEach((j) => {
-        //   console.logy(j);
-        //   j.portrait = require("@/assets/" + j.portrait);
-        // });
+        this.ranking.forEach((i) => {
+          if (i.id == 3) {
+            i.Icon = require("@/assets/" + i.Icon);
+            this.Level3.push(i);
+
+            console.log(this.Level3);
+          } else {
+            i.Icon = require("@/assets/" + i.Icon);
+            this.Level4.push(i);
+            console.log(this.Level4);
+          }
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -72,13 +93,14 @@ export default {
 }
 .gift-count {
   height: 89.5vh;
-  margin-left: 28px;
+  margin-left: 0.56rem;
+  display: flex;
 }
 .gift-left {
-    height: 100%;
+  height: 100%;
   width: 1.44rem;
-  margin-top: 18px;
-  border-right: 1px solid #0000000a;
+  margin-top: 0.36rem;
+  border-right: 0.02rem solid #0000000a;
 }
 .gift-left li {
   margin-top: 0.6rem;
@@ -88,5 +110,71 @@ export default {
   font-weight: 400;
   color: #9c9eb9;
   line-height: 0.36rem;
+}
+.gift-right {
+  margin-top: 0.32rem;
+  margin-left: 0.18rem;
+}
+.gift-right h2 {
+  width: 1.14rem;
+  height: 0.4rem;
+  font-size: 0.28rem;
+  font-family: PingFangSC-Semibold, PingFang SC;
+  font-weight: 600;
+  color: #2d3142;
+  line-height: 0.4rem;
+}
+
+.gift-third {
+  width: 222px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.gift-third img {
+  width: 1.32rem;
+  height: 1.32rem;
+}
+.gift-third p{
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #4c5980;
+  line-height: 0.36rem;
+  margin-top: 0.16rem;
+}
+.gift-third li {
+  display: flex;
+  flex-direction: column;
+  width: 1.32rem;
+  margin-top: 0.3rem;
+  margin-left: 0.16rem;
+  
+}
+.gift-fourth {
+  width: 4.44rem;
+  display: flex;
+  flex-wrap: wrap;
+}
+.gift-fourth img {
+  width: 1.32rem;
+  height: 1.32rem;
+}
+.gift-fourth p{
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #4c5980;
+  line-height: 0.36rem;
+  margin-top: 0.16rem;
+}
+.gift-fourth li {
+  display: flex;
+  flex-direction: column;
+  width: 1.32rem;
+  margin-top: 0.3rem;
+  margin-left: 0.16rem;
+}
+.four {
+  margin-top: 1rem;
 }
 </style>
